ee3335e58dcb76443a7b0904763e126261d0b06a
Wiktor Garbarek <wektorgarbarek@gmail.com>
2017-12-06 23:11:58 +0100

    Done database connection and CRUD

--- Python/l8python/database.py
+++ Python/l8python/database.py
@@ -12,42 +12,63 @@ class Album:
     def lend_to(self, to_who):
         self.is_lend = True
         self.to_who = to_who
-    def accept_back(self):
+
+    def get_back(self):
         self.is_lend = False
         self.to_who = None
 
+def setup_connection(func):
+    def setup_decorator(self, *args, **kwargs):
+        db_conn, c = self._make_conn()
+        func(self, c, *args, **kwargs)
+        self._end_conn(db_conn)
+    return setup_decorator
+
 class Data:
+
+    def _make_conn(self):
+        db_conn = sqlite3.connect('albums.db')
+        c = db_conn.cursor()
+        return db_conn, c
+
+    def _end_conn(self, db_conn):
+        db_conn.commit()
+        db_conn.close()
+
     def __init__(self):
         try:
             with open("albums.db") as f:
-                self.db_connection = sqlite3.connect('albums.db')
+                pass
         except OSError:
             self.db_connection = sqlite3.connect('albums.db')
+            c = self.db_connection.cursor()
             c.execute("""CREATE TABLE albums (id, title, authors, year, is_lend, to_who)""")
+            self.db_connection.commit()
+            self.db_connection.close()
 
-    def insert_one(self, album):
-        c = self.db_connection.cursor()
-        c.execute("""INSERT INTO albums VALUES ({},{},{},{},{},{})""".format(album.id, album.title, album.authors,
-                                                                             album.year, album.is_lend, album.to_who))
-    def remove_one(self, album):
-        c = self.db_connection.cursor()
-        c.execute("""DELETE FROM albums WHERE id={} AND title={} AND authors={} AND year={}""".format(album.id, album.title,
+    @setup_connection
+    def insert_one(self, c, album):
+        c.execute("""INSERT INTO albums VALUES (?,?,?,?,?,?)""", (album.id, album.title, album.authors,
+                                                                 album.year, album.is_lend, album.to_who))
+    @setup_connection
+    def remove_one(self, c, album):
+        c.execute("""DELETE FROM albums WHERE id=? AND title=? AND authors=? AND year=?""", (album.id, album.title,
                                                                                                       album.authors, album.year))
-
-    def find_all(self, query):
-        c = self.db_connection.cursor()
-        c.execute("""SELECT * FROM albums WHERE id LIKE '%{0}%' OR title LIKE '%{0}%' OR authors LIKE '%{0}%'
-                     OR year LIKE '%{0}%' OR is_lend LIKE '%{0}%' OR to_who LIKE '%{0}%';""")
+    @setup_connection                                                                                                  
+    def find_all(self, c, query):
+        c.execute("""SELECT * FROM albums WHERE id LIKE '%?%' OR title LIKE '%?%' OR authors LIKE '%?%'
+                     OR year LIKE '%?%' OR is_lend LIKE '%?%' OR to_who LIKE '%?%';""", tuple([query]*6))
         return c.fetchall()
 
-class ContactBrowser:
+class DatabaseBrowser:
     def __init__(self):
         pass
 
-    def input_handler(search_entry, new_key):
+    def _input_handler(search_entry, new_key):
         if new_key == 65288:
             return search_entry[:-1]
         return search_entry + chr(new_key) if new_key <= 128 else search_entry
 
-    def do_search(self):
+    def do_search(search_entry, new_key):
+        return Data.find_all()
         pass
--- Python/l8python/gui.py
+++ Python/l8python/gui.py
@@ -7,7 +7,8 @@ NO_OF_RESULTS = 10
 
 class GUI(Gtk.Window):
 
-    def __init__(self):
+    def __init__(self, data):
+        self.data = data
         Gtk.Window.__init__(self, title="Album Manager")
         self.set_size_request(400, 200)
 
@@ -23,13 +24,13 @@ class GUI(Gtk.Window):
         self.search_entry = Gtk.Entry()
         self.box.pack_start(self.search_entry, True, True, 0)
 
-        self.buttons = [Gtk.Button("") for i in range(NO_OF_RESULTS)]
-        for button in self.buttons:
-            self.box.pack_start(button, True, True, 0)
+        self.search_result = Gtk.Label("\n"*NO_OF_RESULTS)
+        self.box.pack_start(self.search_result, True, True, 0)
 
-        #self.search_result = Gtk.Label("")
-        #self.box.pack_start(self.search_result, True, True, 0)
-        #for
+        self.edit_button = Gtk.Button("Edit them!")
+        self.edit_button.connect_after('clicked', self.on_edit_clicked)
+
+
         self.show_all()
 
 
@@ -37,12 +38,11 @@ class GUI(Gtk.Window):
         Gtk.main_quit()
 
     def on_key_event(self, widget, event):
-        results = [ContactBrowser.input_handler(self.search_entry.get_text(), event.keyval) for _ in range(10)]
-        for i, result in enumerate(results):
-            if result:
-                self.buttons[i] = Gtk.Button("result")
-                #button.set_label(result)
+        self.search_result.set_label("\n\n".join([DatabaseBrowser.do_search(self.search_entry.get_text(), event.keyval) for _ in range(5)]) + "\nand {} more.".format(len(results) - NO_OF_RESULTS))
         self.show_all()
+
+    def on_edit_clicked(self):
+        pass
 
     def on_clicked_event(self, *args):
         pass
@@ -53,7 +53,11 @@ class SearchResults(Gtk.Window):
 class AlbumEditor(Gtk.Window):
     pass
 
+class App:
+    def main():
+        data = Data()
+        GUI(data)
+        Gtk.main()
 
 if __name__ == "__main__":
-    GUI()
-    Gtk.main()
+    App.main()
