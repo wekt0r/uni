dbeccbf163d140f6695b85f99516b6f9e94f0578
wekt0r <wektorgarbarek@gmail.com>
2017-12-17 00:22:20 +0100

    Trig polynomials fix

--- Analiza Numeryczna/P2/prog/trigonometric_polynomials.jl
+++ Analiza Numeryczna/P2/prog/trigonometric_polynomials.jl
@@ -9,19 +9,25 @@ type TrigonometricPoly
 end
 
 function derivative(t::TrigonometricPoly)
+    n,m = length(t.cs), length(t.ss)
+    cs = hcat(t.cs, repmat([0], max(m - n,0) ))
+    ss = hcat(t.ss, repmat([0], max(n - m,0) ))
     new_ss = [0]
     new_cs = [0]
-    for i in 2:1:max(length(t.cs),length(t.ss));
-        push!(new_ss, -t.cs[i]/(i-1))
-        push!(new_cs, t.ss[i]/(i-1))
+    for i in 2:1:max(n,m);
+        push!(new_ss, -cs[i]/(i-1))
+        push!(new_cs, ss[i]/(i-1))
     end
     return TrigonometricPoly(new_cs, new_ss)
 end
 
 function value(t::TrigonometricPoly, x)
+    n,m = length(t.cs), length(t.ss)
+    cs = hcat(t.cs, repmat([0], max(m - n,0) ))
+    ss = hcat(t.ss, repmat([0], max(n - m,0) ))
     v = t.cs[1] + t.ss[1]
-    for i in 2:1:max(length(t.cs), length(t.ss))
-        v += t.cs[i]*cos((i-1)x) + t.ss[i]*sin((i-1)x)
+    for i in 2:1:max(n, m)
+        v += cs[i]*cos((i-1)x) + ss[i]*sin((i-1)x)
     end
     return v
 end
